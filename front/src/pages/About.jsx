61ed9fd74891ab348aa5e51427b136fa3f4c6b60
kaganakcelik-school <kaganakcelik.school@gmail.com>
2024-05-20 20:28:00 -0500

    added everything else

--- front/src/pages/About.jsx
+++ front/src/pages/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>A really cool about page</h1>
+      <p className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aliquam doloribus fuga, rem, eos nostrum, iste tenetur architecto libero hic laudantium alias. Ipsum doloribus accusamus cumque, ab alias qui quia! </p>
+      <p className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis et labore repellendus, voluptate commodi, nostrum fugiat nulla impedit enim incidunt quo harum, ex iste nobis. Architecto aut officia autem nesciunt!</p>
+    </div>
   )
 }
 
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-700'>A really cool home page</h1>
+      <p className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, dolor laborum culpa quia optio cumque quos, odio, magni voluptatibus ea incidunt ipsum! Possimus libero nam sint eligendi expedita nisi. Laudantium?</p>
+      <p className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, corporis cupiditate ad labore repellendus quis cum! Ducimus deleniti culpa error odio illo, necessitatibus repellendus. Doloremque eos neque dolorem totam voluptatibus?</p>
+      <p className='mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt officia sint, qui doloribus odio repudiandae commodi esse modi ab adipisci! Iure, nemo sit. Et eaque illum beatae necessitatibus, laborum ratione?</p>
+    </div>
   )
 }
 
